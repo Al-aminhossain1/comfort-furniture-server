7137b6a06af684fdd8b6d363bce4f91c35fd2d4f
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-30 14:52:25 +0600

    get product data from database

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 // use middleware
 app.use(cors());
 app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8o1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouse').collection('product');
+
+        //    Get user: 
+
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('comfort furniture server is running');
 })
 
 
-app.get('/product', (req, res) => {
-    res.send("the product is found her and to deploy this ")
-})
+
 
 app.listen(port, () => {
     console.log('server is running');
